4ea97b1eeaf3d5e647c48062a4a56b83eb58f646
Yuhong Liu <yuhongliu.ca@gmail.com>
2021-12-18 21:15:30 -0800

    Product item UI improvement

--- force-app/main/default/lwc/cartApp/cartApp.js
+++ force-app/main/default/lwc/cartApp/cartApp.js
@@ -1,11 +1,12 @@
 import { LightningElement, wire, api, track  } from 'lwc';
 
 export default class CartApp extends LightningElement {
-    @track groceryProducts = [{name: "Apple",image:"https://images.unsplash.com/photo-1611574474484-ced6cb70a2cf?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTYzOTc4ODk3NQ&ixlib=rb-1.2.1&q=85",price:2.75,quantity:0,inc:"AppleInc",dec:"AppleDec",category:"NewArrival"},
-    {name: "Pear",image:"https://images.unsplash.com/photo-1531746808781-ce4c1b3a5ff2?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTYzOTc4Nzg0Mw&ixlib=rb-1.2.1&q=85",price:1.75,quantity:0,inc:"PearInc",dec:"PearDec",category:"BestSeller"},
-    {name: "Banana",image:"https://images.unsplash.com/photo-1623810836868-057b23aef3aa?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTYzOTc4Nzg3OA&ixlib=rb-1.2.1&q=85",price:0.99,quantity:0,inc:"BananaInc",dec:"BananaDec",category:"OnSale"},
-    {name: "Orange",image:"https://images.unsplash.com/photo-1606050495266-ab4f2615cea5?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTYzOTc4NzkzMQ&ixlib=rb-1.2.1&q=85",price:1.09,quantity:0,inc:"OrangeInc",dec:"OrangeDec",category:"OnSale"},
-    {name: "Grape",image:'https://images.unsplash.com/photo-1599819177626-b50f9dd21c9b?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTYzOTc4ODAxMg&ixlib=rb-1.2.1&q=85',price:3.99,quantity:0,inc:"GrapeInc",dec:"GrapeDec",category:""}
+    @track groceryProducts = [
+    {name: "Apple",image:"https://images.unsplash.com/photo-1611574474484-ced6cb70a2cf?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTYzOTc4ODk3NQ&ixlib=rb-1.2.1&q=85",price:2.75,quantity:1000,inc:"AppleInc",dec:"AppleDec",category:"NewArrival",description:"One pound apple"},
+    {name: "Pear",image:"https://images.unsplash.com/photo-1531746808781-ce4c1b3a5ff2?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTYzOTc4Nzg0Mw&ixlib=rb-1.2.1&q=85",price:1.75,quantity:500,inc:"PearInc",dec:"PearDec",category:"BestSeller",description:"One pound pear"},
+    {name: "Banana",image:"https://images.unsplash.com/photo-1623810836868-057b23aef3aa?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTYzOTc4Nzg3OA&ixlib=rb-1.2.1&q=85",price:0.99,quantity:600,inc:"BananaInc",dec:"BananaDec",category:"OnSale",description:"A bunch of bananas(4 pcs)"},
+    {name: "Orange",image:"https://images.unsplash.com/photo-1606050495266-ab4f2615cea5?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTYzOTc4NzkzMQ&ixlib=rb-1.2.1&q=85",price:1.09,quantity:200,inc:"OrangeInc",dec:"OrangeDec",category:"OnSale",description:"One bag with 4 oranges"},
+    {name: "Grape",image:'https://images.unsplash.com/photo-1599819177626-b50f9dd21c9b?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTYzOTc4ODAxMg&ixlib=rb-1.2.1&q=85',price:3.99,quantity:700,inc:"GrapeInc",dec:"GrapeDec",category:"",description:"One pound grapes"}
     ];
     @track cartItems;
     @track displayProduct = true;
--- force-app/main/default/lwc/productItem/productItem.js
+++ force-app/main/default/lwc/productItem/productItem.js
@@ -1,6 +1,47 @@
-import { LightningElement, api} from 'lwc';
+import { LightningElement, api, track} from 'lwc';
 
 export default class ProductItem extends LightningElement {
     @api 
-    gproduct = {name: "Pear",image:"https://en.wikipedia.org/wiki/Pear#/media/File:Assortment_of_pears.jpg",price:1.75,quantity:0,inc:"PearInc",dec:"PearDec",category:"BestSeller"};
+    gproduct;
+
+    @track
+    itemQuantity = 0;
+
+    addProductItem(event) {
+        if(this.itemQuantity < this.gproduct.quantity)
+            this.itemQuantity++;
+    }
+
+    reduceProductItem(event) {
+        if(this.itemQuantity > 0)
+            this.itemQuantity--;
+    }
+
+    handleQuantityChange(event) {
+        let n = parseInt(event.target.value);
+        if(n <= this.gproduct.quantity && n >= 0) {
+            this.itemQuantity = n;
+        } 
+    }
+
+    addFruitItem(event) {
+        let itemId = event.target.id;
+        let index = itemId.indexOf("Inc");
+        let itemName = itemId.substring(0,index);
+        console.log("item="+itemName);
+        this.dispatchEvent(new CustomEvent('additem', {
+            detail: itemName,  
+            bubbles: true
+        }));
+    }
+
+    removeFruitItem(event) {
+        let itemId = event.target.id;
+        let index = itemId.indexOf("Dec");
+        let itemName = itemId.substring(0,index);
+        this.dispatchEvent(new CustomEvent('removeitem', {
+            detail: itemName,  
+            bubbles: true
+        }));
+    }
 }
